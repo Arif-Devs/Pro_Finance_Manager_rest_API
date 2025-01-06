40f3960822664a066869d0b3d3cc99b8c212da7d
Arifur Rahman <arifur.sew@gmail.com>
2025-01-06 20:14:17 +0600

    swagger live server add

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import router from './routes/index.js';
+import YAML from 'yamljs'
+import swaggerUi from 'swagger-ui-express';
+const swaggerSpec = YAML.load('./docs/swagger.yaml');
+import cors from 'cors'
+import morgan from 'morgan'
 
 dotenv.config();
 const app = express();
+
+const middleware = [cors(), morgan('dev'), express.json(), express.urlencoded({ extended: false })]
+
+//Middleware
+app.use(middleware)
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 
 
 //Routs
