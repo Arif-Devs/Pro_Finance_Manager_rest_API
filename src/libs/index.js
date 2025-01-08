b8f51c83cc2f12bcd56a5dc50cf4e312c829a809
Arifur Rahman <arifur.sew@gmail.com>
2025-01-08 21:00:48 +0600

    creatr role route done

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,4 +1,5 @@
 import UserLibs from "./User.js";
 import tokenLibs from "./token.js";
 import permissionLibs from './permission.js'
-export {UserLibs, tokenLibs, permissionLibs}
+import roleLibs from "./role.js";
+export {UserLibs, tokenLibs, permissionLibs, roleLibs}
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 const router = express.Router();
 import authController from '../api/v1/controller/auth/index.js';
 import PermissionController from '../api/v1/controller/permission/index.js';
+import RoleController from '../api/v1/controller/role/index.js';
 import {authRequest} from '../request/index.js'
 import { requestValidator, authenticate, authorization } from '../middleware/index.js';
 import  {permissionRequest}  from '../request/index.js';
@@ -19,11 +20,12 @@ router.get('/health', (_req, res) =>
 
 //Permission Routes ->
  router.route('/permissions')  
-  //TODO add authentication and authorization middleware -> authenticate, authorization(['create-permission'])
   .post( authenticate, authorization(['create-permission']), permissionRequest.permissionCreateRequest,requestValidator,PermissionController.create)
 
 
-
+//Role route
+router.route('/roles')
+.post(RoleController.create)
 
 
 
