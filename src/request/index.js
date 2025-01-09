ad62690edc108969a78d407f30b8e61322cef57a
Arifur Rahman <arifur.sew@gmail.com>
2025-01-09 21:00:37 +0600

    create user request validator add

--- src/request/index.js
+++ src/request/index.js
@@ -1,12 +1,14 @@
 import authRequest from "./authenticateRequest.js";
 import permissionRequest from './permissionRequest.js'
 import RoleRequest from "./roleRequest.js";
+import UserRequest from './userRequest.js'
 
 
 
 export  {
     authRequest,
     permissionRequest,
-    RoleRequest
+    RoleRequest,
+    UserRequest
 }
 
