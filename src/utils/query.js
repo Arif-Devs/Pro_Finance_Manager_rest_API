18ce3f68e7c25bef6261bf0d189d11b4a6274a43
Arifur Rahman <arifur.sew@gmail.com>
2025-01-10 18:59:28 +0600

    query string generate function

---
+++ src/utils/query.js
@@ -0,0 +1,23 @@
+// Generate sort type
+const generateSortType = (sortType) => {
+  return sortType === 'desc' ? -1 : 1;
+};
+
+//Generate query string
+const generateQueryString = (path, query)=>{
+  const encodeQuery = Object.keys(query).map((key)=>encodeURIComponent(key)+'='+encodeURIComponent(query[key])).join('&') 
+  return `${path}?${encodeQuery}`
+}
+
+const generateSelectedItems = (array, defaultArray)=>{
+  
+  
+  let selectedArray = defaultArray
+  if(array.length>0){
+    selectedArray = array.split(',').map((item)=>item.trim())
+  }
+  return selectedArray
+  
+}
+
+export { generateSortType, generateQueryString, generateSelectedItems };
