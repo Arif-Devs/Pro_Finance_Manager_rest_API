e8ed1cbf0ca9c115209497fddaab79114e828d3b
Arifur Rahman <arifur.sew@gmail.com>
2025-01-10 19:01:38 +0600

    get all data

--- src/api/v1/controller/user/userController.js
+++ src/api/v1/controller/user/userController.js
@@ -1,4 +1,8 @@
 import { UserLibs } from "../../../../libs/index.js";
+import {IDQUERY, LIMIT, PAGE, POPULATE, SEARCH, SELECT, SORTBY, SORTTYPE} from '../../../../config/default.js'
+import transformMongooseDocs from '../../../../utils/response.js'
+import {generateAllDataHateoasLinks} from '../../../../utils/hateoas.js'
+import generatePagination from '../../../../utils/pagination.js'
 
 const create = async(req, res, next)=>{
     try {
@@ -18,6 +22,43 @@ const create = async(req, res, next)=>{
     }
 }
 
+//get all
 
+const getAll = async(req, res, next)=>{
+    try {
+    
+        let {limit, page, sortType, sortBy, search, role, select, populate} = req.query
 
-export default {create}
+    //set default value
+    limit = +limit || LIMIT
+    page = +page || PAGE
+    sortType = sortType || SORTTYPE
+    sortBy = sortBy || SORTBY
+    search = search || SEARCH
+    role = role || IDQUERY
+    select = select || SELECT
+    populate = populate || POPULATE
+
+    let{query, totalItems} = await UserLibs.getAllData({search, sortBy, sortType, limit, page, role, select, populate})
+
+    // count total page
+    let totalPage = Math.ceil(totalItems/limit)
+
+    // send res to client
+    let result = {
+        code: 200,
+        message: 'data retrieved success!',
+        data: query.length > 0 ? transformMongooseDocs(query, req.url) : [],    
+        links: generateAllDataHateoasLinks(query, req.url, req._parsedUrl.pathname, page, totalPage, req.query),
+        pagination: generatePagination(totalPage, page, totalItems, limit)
+        
+        
+    }
+    return res.status(200).json({result})
+    
+} catch (error){
+    next (error)
+ }
+}
+
+export default {create, getAll}
