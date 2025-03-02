56aa1cdb10b12e7830278198b4faae082ffd3072
Arif <arifur.sew@gmail.com>
2025-03-02 19:22:37 +0600

    expanse

--- src/libs/expanse.js
+++ src/libs/expanse.js
@@ -1,5 +1,6 @@
 import Expanse from "../model/expanse.js";
 import { accountRelationDataCheck, userRelationDataCheck,categoryRelationDataCheck, serverError } from "../utils/error.js";
+import {generateSelectedItems, generateSortType} from '../utils/query.js'
 
 const checkRelationData = async(userId, categoryId, accountId, authUserId )=>{
     if(userId){
@@ -39,6 +40,66 @@ const createExpanse = async({amount, note, categoryId, accountId, userId})=>{
 }
 
 //get all expanse
+
+const getAll = async({limit, page, sortType, sortBy, search, user, select, populate, account, category, min_price, max_price, fromDate, toDate})=>{
+    try {
+        const sortTypeForDB = generateSortType(sortType)
+        const selectFields = generateSelectedItems(select['_id', 'amount', 'categoryId', 'userId','accountId', 'note', 'createdAt', 'updatedAt'])
+        const populateFields = generateSelectedItems(populate['user', 'category', 'account'])
+
+        let filter = {}
+        if(search) filter.note = {$regex : search , $options : 'i'}
+        if(user) filter.userId = user
+        if(account) filter.accountId = account
+        if(category) filter.categoryId = category
+
+        if(min_price || max_price){
+            filter.amount = {}
+            if(min_price){
+                filter.amount.$gte = min_price
+            }
+            if(max_price){
+                filter.amount.$lte = max_price
+            }
+        }
+        
+        if(fromDate || toDate){
+            filter.update = {}
+            if(fromDate){
+                filter.update.$gte = new Date(fromDate)
+            }
+            if(toDate){
+                filter.update.$lte = new Date(toDate)
+            }
+        }
+
+
+
+
+
+
+
+        
+    } catch (error) {
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 export default {createExpanse, checkRelationData}
